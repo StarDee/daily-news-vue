4a51abf90ba7d87e2ac5d96cc1ba025db9a076b0
StarDee <di.star@foxmail.com>
2017-05-04 14:22:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Daily News
+
+## 声明
+
+本项目仅用于学习使用，所有文字图片等稿件内容均由 [知乎](https://www.zhihu.com/) 提供，获取与共享之行为或有侵犯知乎权益的嫌疑。
+
+
+## apk下载地址
+
+[请点击这里下载](https://pan.baidu.com/s/1boQbp0N)
+
+
+[博客地址]()
+
+
+MIT license.
+
+感谢 [ZhihuDailyPurify](https://github.com/izzyleung/ZhihuDailyPurify/wiki/%E7%9F%A5%E4%B9%8E%E6%97%A5%E6%8A%A5-API-%E5%88%86%E6%9E%90)整理了API
---
+++ src/view/author.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="photo">
+      <img src="http://ooeqdvvze.bkt.clouddn.com/star123.png" alt="">
+    </div>
+    <p class="name">StarDee</p>
+    <ul>
+      <li><a href="https://github.com/stardee/daily-news-vue">项目github地址</a></li>
+      <li><a href="http://stardee.github.io/">我的博客地址</a></li>
+    </ul>
+  </div>
+</template>S
+
+<script>
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  .photo{
+    width: 80px;
+    height: auto;
+    margin: 0 auto;
+    padding: 30px 0;
+    img{
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+  }
+  .name{
+    width: 100px;
+    margin: 0 auto 30px auto;
+    padding-bottom: 2px;
+    color: #999;
+    font-size: 20px;
+    text-align: center;
+    border-bottom: 1px solid #4bb6ae;
+  }
+  ul{
+    width: 150px;
+    margin: 0 auto;
+    font-size: 18px;
+    text-align: center;
+    border-top: 1px solid #b6b6b6;
+    li{
+      padding: 10px;
+      border-bottom: 1px solid #b6b6b6;
+      p{
+        color: #999;
+      }
+      a{
+        color: #2482ab;
+      }
+    }
+  }
+  .btn{
+    margin: 50px auto 0 auto;
+    width: 100px;
+    height: 40px;
+    border: 1px solid #999;
+    border-radius: 10px;
+    color: #b22323;
+    font-size: 18px;
+    text-align: center;
+    line-height: 40px;
+  }
+</style>
